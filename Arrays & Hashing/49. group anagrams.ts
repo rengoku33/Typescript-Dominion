06bfaf5b7d4e4c511f97e4c2de03032df657c6b4
Kiran Vignesh <rajkiranvignesh369@gmail.com>
2025-09-02 00:15:06 +0530

    Update 49. group anagrams.ts

--- Arrays & Hashing/49. group anagrams.ts
+++ Arrays & Hashing/49. group anagrams.ts
@@ -11,3 +11,19 @@ function groupAnagrams(strs: string[]): string[][] {
     }
     return Array.from(map.values())
 };
+
+// good luck with that
+function groupAnagrams(words: string[]): string[][] {
+  const anagramGroups = new Map<string, string[]>();
+
+  for (const word of words) {
+    const frequencyKey = Array.from(word).reduce((frequency, char) => {
+      frequency[char.charCodeAt(0) - 97]++;
+      return frequency;
+    }, Array(26).fill(0)).join("#");
+
+    (anagramGroups.get(frequencyKey) || anagramGroups.set(frequencyKey, []).get(frequencyKey)).push(word);
+  }
+
+  return [...anagramGroups.values()];
+}
